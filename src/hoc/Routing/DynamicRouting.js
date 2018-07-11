d538a6db0fc489c70d2ddd41d489fb8586fe68f7
Serhat Selim <serhatselim@hotmail.com.tr>
2018-07-11 14:25:52 +0300

    dynamic routing
    add class

---
+++ src/hoc/Routing/DynamicRouting.js
@@ -0,0 +1,8 @@
+import React, {Component} from 'react';
+import { withRouter } from 'react-router-dom';
+
+class DynamicRouting extends Component{
+    
+}
+
+export default withRouter(DynamicRouting);
